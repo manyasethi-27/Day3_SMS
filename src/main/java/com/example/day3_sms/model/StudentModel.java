acbdb49e892f0c3fc45ae44ee88e66ecf09cf705
manyasethi-27 <manya.sethi_cs23@gla.ac.in>
2026-01-20 12:50:12 +0530

    added mongodb connection

---
+++ src/main/java/com/example/day3_sms/model/StudentModel.java
@@ -0,0 +1,19 @@
+package com.example.day3_sms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "students")
+
+public class StudentModel{
+
+    private String id;
+    private String name;
+    private int age;
+    private String email;
+}
